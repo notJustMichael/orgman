cf935e9aff334608adeca3d923ff665c90f5fcf6
notJustMichael <33413974+notJustMichael@users.noreply.github.com>
2019-09-04 08:36:47 +0200

    Update OrganisationFactoryTest.java

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/org/OrganisationFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/org/OrganisationFactoryTest.java
@@ -12,6 +12,7 @@ public class OrganisationFactoryTest {
     @Test
     public void buildOrganisation() {
         Assert.assertNotNull(organisation);
+        Assert.assertNotNull(gender.getOrgCode());
         Assert.assertNotEquals("ABC Group", organisation.getOrgName());
     }
 }
